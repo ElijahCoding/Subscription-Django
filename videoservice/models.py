dd82b91ea7fd6a51c1e253eb16042a52fd3ad88c
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-26 17:09:24 +0300

    user-membership relationship built

--- videoservice/models.py
+++ videoservice/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 MEMBERSHIP_CHOICES = (
     ('Enterprise', 'ent'),
@@ -8,9 +9,21 @@ MEMBERSHIP_CHOICES = (
 
 class Membership(models.Model):
     slug = models.SlugField()
-    membership_type = models.CharField(choices=MEMBERSHIP_CHOICES, max_length=30)
+    membership_type = models.CharField(
+        choices=MEMBERSHIP_CHOICES,
+        max_length=30,
+        default='Free',
+    )
     price = models.IntegerField(default=15)
     stripe_plan_id = models.CharField(max_length=40)
 
     def __str__(self):
         return self.membership_type
+
+class UserMembership(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    stripe_custom_id = models.CharField(max_length=40)
+    membership = models.ForeignKey(Membership, on_delete=models.SET_NULL, null=True)
+
+class Subscription(models.Model):
+    pass
