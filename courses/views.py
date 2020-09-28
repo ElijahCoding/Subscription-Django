4075001d2f1bc4a2160a6bf8e09bc7928b81f16a
ElijahCoding <cuiyeqing960904@gmail.com>
2020-09-28 23:37:27 +0300

    building views

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Course
 
-# Create your views here.
+class CourseListView(ListView):
+    model = Course
+
+class CourseDetailView(DetailView):
+    model = Course
